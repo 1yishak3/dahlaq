78ebe373f2abf2bbbc2598cc34358cfb2fdc3fb9
Yishak Tofik Mohammed <25166250+yitzhaqm@users.noreply.github.com>
2018-05-30 05:15:03 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
+import c from "./constants"
 export const environment={
-  production:false,
+  production:true,
   firebase:{
-    apiKey: "AIzaSyCYT5qaezgIxItyCT_idaM0rXNnKA9eBMY",
-    authDomain: "dahlaq-c7e0f.firebaseapp.com",
-    databaseURL: "https://dahlaq-c7e0f.firebaseio.com",
-    projectId: "dahlaq-c7e0f",
-    storageBucket: "dahlaq-c7e0f.appspot.com",
-    messagingSenderId: "500593695235"
+    apiKey: c.A,
+    authDomain: c.D,
+    databaseURL: c.U,
+    projectId: c.I,
+    storageBucket: c.B,
+    messagingSenderId: c.S
   }
   };
